63ff426a400d8013fd3911f7e0df2cd772859651
petarPAN <petarburic554@gmail.com>
2021-01-25 21:29:53 +0100

    Retrieving data from particular city

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
-const fetchData2 = async () => {
+const input = document.querySelector('input');
+
+const fetchData = async (searchTerm) => {
     const response = await axios.get('http://api.openweathermap.org//data/2.5/weather?', {
         params: {
-            q: 'London',
+            q: searchTerm,
             appid: '06b8dbc1b469c1a8509ab3fd20ded3d0',
             units: 'metric'
         }
     });
 
-    console.log(response.data)
+    if (response.data.Error) {
+        return [];
+    }
+
+    return response.data
 }
 
+let timeoutId;
+const onInput = event => {
+    if (timeoutId) {
+        clearInterval(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+        fetchData(event.target.value);
+    }, 500)
+}
+
+input.addEventListener('input', onInput);
 
